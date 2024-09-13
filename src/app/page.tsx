8afa872dcf46564b6d214e5473c9700c78af3020
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-09-12 21:00:49 -0300

    feat: pesquisa de jogos
    criado a página responsável por exibir os jogos buscados pelo formulário de pesquisa.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,8 +65,8 @@ export default async function Home() {
 
         <h2 className="text-lg  font-bold mt-8 mb-5">Jogos para conhecer</h2>
         
-        <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {data.map((item) => (
+        <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+          {data.map( (item) => (
             <GameCard key={item.id} data={item} />
           ))}
         </section>
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -18,14 +18,14 @@ export function Input(){
     return(
         <form 
             onSubmit={handleSearch} 
-            className="w-full bg-slate-200 my-5 flex gap-2 items-center justify-between rounded-lg p-2"
+            className="w-full bg-slate-100 my-5 flex gap-2 items-center justify-between rounded-lg p-2"
         >
             <input 
                 type="text" 
                 placeholder="Procurando algum jogo?..."
                 value={input}
                 onChange={(event) => setInput(event.target.value)}
-                className="bg-slate-200 outline-none w-11/12"
+                className="bg-slate-100 outline-none w-11/12"
             />
 
             <button type="submit">
